c92505053663017b43e7ffe627c5900b5aa38a53
Luan Lisboa <lluan4593@gmail.com>
2026-04-26 21:13:41 -0300

    commit inicial

---
+++ src/main/java/com/lisboa/agendamento_notificacao_api/AgendamentoNotificacaoApiApplication.java
@@ -0,0 +1,15 @@
+package com.lisboa.agendamento_notificacao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoNotificacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoNotificacaoApiApplication.class, args);
+
+		System.out.println("ola mundo");
+	}
+
+}
